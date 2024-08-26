dc04607756f41d618fb483de90a3c2ff4d5dd3f2
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-08-26 10:26:19 +0800

    initial commit

---
+++ src/app/about/head.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+export default function head() {
+  return (
+    <>
+      <title>About Page</title>
+      <meta content="with=device-width,initial-scale=1" name="viewport" />
+      <meta name="description" content="Created by Lyndell" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
