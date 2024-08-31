46ae7199278dd20a8a94830f08072d6517571a7f
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-08-31 17:08:08 +0800

    add unit test coverage for layout main

--- jest.config.js
+++ jest.config.js
@@ -23,23 +23,23 @@ const config = {
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
     "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-
+    "\\.(scss|sass|css)$": "identity-obj-proxy",
     // Handle CSS imports (without CSS modules)
-    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    // "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
 
     // Handle image imports
-    // https://jestjs.io/docs/webpack#handling-static-assets
-    "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/__mocks__/fileMock.js`,
+    // // https://jestjs.io/docs/webpack#handling-static-assets
+    // "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/__mocks__/fileMock.js`,
 
-    // Handle module aliases
-    "^@/components/(.*)$": "<rootDir>/components/$1",
+    // // Handle module aliases
+    // "^@/components/(.*)$": "<rootDir>/components/$1",
 
-    // Handle @next/font
-    "@next/font/(.*)": `<rootDir>/__mocks__/nextFontMock.js`,
-    // Handle next/font
-    "next/font/(.*)": `<rootDir>/__mocks__/nextFontMock.js`,
-    // Disable server-only
-    "server-only": `<rootDir>/__mocks__/empty.js`,
+    // // Handle @next/font
+    // "@next/font/(.*)": `<rootDir>/__mocks__/nextFontMock.js`,
+    // // Handle next/font
+    // "next/font/(.*)": `<rootDir>/__mocks__/nextFontMock.js`,
+    // // Disable server-only
+    // "server-only": `<rootDir>/__mocks__/empty.js`,
   },
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
