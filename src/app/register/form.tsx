473ec8b556b2876b5d60f84012a3b16827746fe9
Lyndell Dobluis <lyndell.dobluis@gmail.com>
2024-08-31 14:37:06 +0800

    add registration screen

--- src/app/register/form.tsx
+++ src/app/register/form.tsx
@@ -1,15 +1,52 @@
-import React from "react";
-
+'use client'
+import React,{useState} from "react";
+import { useRouter } from 'next/navigation';
+import { Input, Button,Label } from "@/components/ui";
 export const RegisterForm = () => {
-  const onSubmit = (e: React.FormEvent) => {
+  const [email,setEmail]=useState('')
+  const [password,setPassword]=useState('')
+  const { push } = useRouter();
+  const onSubmit = async(e: React.FormEvent) => {
     e.preventDefault();
-    console.log("test");
+    try{
+      // const response=await fetch('/api/register',{
+      //   method:'POST',
+      //   body:JSON.stringify({
+      //     email,password
+      //   }),
+      //   headers:{
+      //     'Content-Type':'application/json'
+      //   }
+      // })
+      // if(response.ok){
+
+      // }
+      push('/')
+    }catch(error){
+
+    }
   };
   return (
-    <form>
-      <input />
-      <input />
-      <button>Register</button>
+    <form onSubmit={onSubmit}
+    className="space-y-12 w[400px]">
+
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+      </div>
+
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      </div>
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <Label htmlFor="password">Confirm Password</Label>
+        <Input id="password" type="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      </div>
+      <div className="w-full">
+        <Button className="w-full" size={"lg"}>Register</Button>
+      </div>
+ 
     </form>
   );
 };
